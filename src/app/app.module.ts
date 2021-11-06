af7c7986a0395eb5b4d6f981a95c4b95aa2377b7
bmeltz <bennymeltzer@gmail.com>
2021-11-06 12:49:26 -0500

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ShippingFormComponent } from './shipping-form/shipping-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ShippingFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
