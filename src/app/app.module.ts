9658514c1d67ed3251ce680b794a1894c9d0b7ed
benny.meltzer <benny.meltzer@brilligent.com>
2021-11-19 21:01:56 -0600

    Ok so NOW you can actually use mdb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { SuccessComponent } from './success/success.component';
 import { FailureComponent } from './failure/failure.component';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // If You need animations
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,7 +37,9 @@ import { HomeComponent } from './home/home.component';
         path: '**',
         component: HomeComponent
       }
-    ], { useHash: true })
+    ], { useHash: true }),
+    MDBBootstrapModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
