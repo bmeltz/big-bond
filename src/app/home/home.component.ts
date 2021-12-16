af5fc640d1a6754308031e5b3f30b6f0cb0a1936
benny.meltzer <benny.meltzer@brilligent.com>
2021-12-16 01:41:51 -0600

    WIP parallax stuff etc

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,15 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const sky = window.document.getElementById('sky');
+    const title = window.document.getElementById('top_text');
+
+    window.addEventListener('scroll', function(){
+      let value = window.scrollY;
+      console.log(value);
+      sky.style.top = value * -.05 + 'px';
+      title.style.top = value * -.3 + 'px';
+    });
   }
 
 }
