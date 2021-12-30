fcd7829365d6963d45cd153ad465318b2bbd43e2
benny.meltzer <meltzerbenny@gmail.com>
2021-12-30 01:48:18 -0600

    updated production env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  production: true,
-  stripe_key: 'pk_test_51HSxmHFHabj9XRH6FRDABkTg4M5jSU7ZPKXismUzYytZh8JkfxqiwjtVDSlbcAR4kQ8U6Yy19RQCoSjsspXVmhzf002G4ny8sA'
+  production: false,
+  STRIPE_PUBLISHABLE_KEY: "pk_test_51JHiV9L2xwG7RCwgbcBanu6hjEI5ZZqdMhC6bHemx86O7OyJwXzYT6DEQ20MNiZH9nh7wZfl4Hn20aW4bguu3PlD002J1nMdsY",
+  PRICE:"price_1JmrzpL2xwG7RCwge5NhXYWH",
+  PAYMENT_METHOD_TYPES: "card"
 };
