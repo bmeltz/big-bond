6caeb109531c762c690b9e6e7714b002896a8c69
benny.meltzer <benny.meltzer@brilligent.com>
2022-02-17 23:14:34 -0600

    added no ceilings | no floors to bottom of most pages. good enough for now

---
+++ src/app/components/no-ceil-floor/no-ceil-floor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'no-ceil-floor',
+  templateUrl: './no-ceil-floor.component.html',
+  styleUrls: ['./no-ceil-floor.component.css']
+})
+export class NoCeilFloorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
