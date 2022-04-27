049274342d4aaee27a288695d1c2a3fbe0a87e6f
benny.meltzer <benny.meltzer@brilligent.com>
2022-04-26 22:59:50 -0500

    added a more detailed packing list

---
+++ src/app/pages/packing-list/detailed/detailed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'detailed',
+  templateUrl: './detailed.component.html',
+  styleUrls: ['./detailed.component.css']
+})
+export class DetailedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
