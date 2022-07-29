97c8344be8112e6c77b23de07e39293568a73546
benny.meltzer <benny.meltzer@brilligent.com>
2022-07-29 00:50:55 -0500

    WIP: first cut of a video background on the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -90,7 +90,8 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
 
     const sky = window.document.getElementById('sky');
-
+    const video = window.document.getElementById('backgound-vid') as HTMLVideoElement;
+    video.play();
     window.addEventListener('scroll', function(){
       let value = window.scrollY;
       sky.style.top = value * .5 + 'px';
