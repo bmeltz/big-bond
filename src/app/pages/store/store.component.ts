d3b1cab100f702988ee18e61cb88606cfdb7d5a9
benny.meltzer <benny.meltzer@brilligent.com>
2022-08-23 00:16:49 -0500

    got a very very hacky solution but it seems promising

--- src/app/pages/store/store.component.ts
+++ src/app/pages/store/store.component.ts
@@ -1,4 +1,4 @@
-import { Component, Renderer2, OnInit, Inject, OnDestroy, AfterViewInit } from '@angular/core';
+import { Component, Renderer2, OnInit, Inject, OnDestroy, AfterViewInit, HostListener } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
@@ -8,26 +8,42 @@ import { DOCUMENT } from '@angular/common';
 })
 export class StoreComponent implements AfterViewInit {
   
+  screenHeight: number;
+  screenWidth: number;
   constructor(private renderer2: Renderer2,
     @Inject(DOCUMENT) private document: Document) {
+      this.getScreenSize();
+  }
 
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+        this.screenHeight = window.innerHeight;
+        this.screenWidth = window.innerWidth;
+        var iFrameID = <HTMLIFrameElement>document.getElementById('frame');
+        if(iFrameID) {
+              // here you can make the height, I delete it first, then I make it again
+              // iFrameID.height = "";
+              iFrameID.style.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
+    
+        }
   }
 
   public ngAfterViewInit(): void {
-
-    // const storeId = 78062460;
-    // const script = this.renderer2.createElement('script');
-    // script.setAttribute('type', 'text/javascript');
-    // script.setAttribute('charset', 'utf-8');
-    // script.setAttribute('data-cfasync', 'false');
-    // script.setAttribute('id', 'testScript');
-    // script.setAttribute('src', `https://app.ecwid.com/script.js?${storeId}&data_platform=code&data_date=2020-02-17`);
-    // script.onload = this.injectEcwidProductBrowser(storeId);
-    // this.renderer2.appendChild(document.body, script);
     
   }
 
-  public iframeLoaded() {
+  private delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+  public async iframeLoaded() {
+    // this is suuuper hacky. there's a race condition with the iframe itself loading vs the contents of the
+    // iframe loading. I need a non hacky solution for this.
+    // it feels slow (4 seconds slow), but mainly i cant guarantee that the contents of the store won't take
+    // longer than 4 seconds to load, and in that case the only way to get the full store to show as its currently
+    // coded is to resize the window, which isn't even possible on mobile.
+    await this.delay(4000);
     var iFrameID = <HTMLIFrameElement>document.getElementById('frame');
     if(iFrameID) {
           // here you can make the height, I delete it first, then I make it again
@@ -35,19 +51,6 @@ export class StoreComponent implements AfterViewInit {
           iFrameID.style.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
     } 
     console.log('what dis', iFrameID.style.height);
-    // iFrameID.style.setProperty('height', iFrameID.height);
-}
-
-  private injectEcwidProductBrowser(storeId) {
-    return () => {
-      const ecwidBrowserScript = this.renderer2.createElement('script');
-      ecwidBrowserScript.setAttribute('type', 'text/javascript');
-      ecwidBrowserScript.setAttribute('charset', 'utf-8');
-      ecwidBrowserScript.setAttribute('id', "id=my-store-${storeId}");
-      ecwidBrowserScript.text = `xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list");`;
-      this.renderer2.appendChild(document.body, ecwidBrowserScript);
-    };
   }
-
 
 }
