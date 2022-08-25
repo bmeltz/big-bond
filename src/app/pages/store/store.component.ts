8422ed90d514e61c48306a679cdd7695dd23bd67
benny.meltzer <benny.meltzer@brilligent.com>
2022-08-25 00:02:29 -0500

    housekeeping

--- src/app/pages/store/store.component.ts
+++ src/app/pages/store/store.component.ts
@@ -1,5 +1,4 @@
-import { Component, Renderer2, OnInit, Inject, OnDestroy, AfterViewInit, HostListener } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'store',
@@ -7,50 +6,39 @@ import { DOCUMENT } from '@angular/common';
   styleUrls: ['./store.component.scss']
 })
 export class StoreComponent implements AfterViewInit {
-  
-  screenHeight: number;
-  screenWidth: number;
-  constructor(private renderer2: Renderer2,
-    @Inject(DOCUMENT) private document: Document) {
-      this.getScreenSize();
+  constructor() {
   }
 
-
-  @HostListener('window:resize', ['$event'])
-  getScreenSize(event?) {
-        this.screenHeight = window.innerHeight;
-        this.screenWidth = window.innerWidth;
-        var iFrameID = <HTMLIFrameElement>document.getElementById('frame');
-        if(iFrameID) {
-              // here you can make the height, I delete it first, then I make it again
-              // iFrameID.height = "";
-              iFrameID.style.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
-    
-        }
-  }
-
+  // this is such a hacky solution to get a good iframe height after the ecwid store has fully loaded.
+  // I've spent days reading every stack overflow and obscure blog post about how to do this and things i've found
+  // while promising do not work for my exact situation so unfortunately here we are. maybe someday a 
+  // legit solution will occur to me, but for now i'm gonna settle and move on to other dumpster fires.
   public ngAfterViewInit(): void {
+    // let stopChecking: (current: string, possible_new: string) => boolean = function(current: string, possible_new: string): boolean{
+    //   // definitely not done if any of these are true
+    //   if(current == '' || possible_new == '' || current.slice(-2) != 'px' || possible_new.slice(-2) != 'px') {
+    //     return false;
+    //   }
+    //   return +(possible_new.slice(0, -2)) > +(current.slice(0, -2));
+    // };
+    const interval = setInterval(function() {
+      var iFrameID = <HTMLIFrameElement>document.getElementById('frame');
+      if(iFrameID) {
+
+        // here you can make the height, I delete it first, then I make it again
+        // iFrameID.height = "";
+        let current = iFrameID.style.height;
+        let possible_new = iFrameID.contentWindow.document.body.scrollHeight + "px";
+        console.log(current, possible_new);
+        iFrameID.style.height = possible_new;
+
+        //TODO: get this to work. need to figure out a good way to stop checking
+        // if(stopChecking(current, possible_new)){
+          //clearInterval()
+        // }
+            
+      } 
+    }, 1000);
     
   }
-
-  private delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
-  }
-
-  public async iframeLoaded() {
-    // this is suuuper hacky. there's a race condition with the iframe itself loading vs the contents of the
-    // iframe loading. I need a non hacky solution for this.
-    // it feels slow (4 seconds slow), but mainly i cant guarantee that the contents of the store won't take
-    // longer than 4 seconds to load, and in that case the only way to get the full store to show as its currently
-    // coded is to resize the window, which isn't even possible on mobile.
-    await this.delay(4000);
-    var iFrameID = <HTMLIFrameElement>document.getElementById('frame');
-    if(iFrameID) {
-          // here you can make the height, I delete it first, then I make it again
-          // iFrameID.height = "";
-          iFrameID.style.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
-    } 
-    console.log('what dis', iFrameID.style.height);
-  }
-
 }
