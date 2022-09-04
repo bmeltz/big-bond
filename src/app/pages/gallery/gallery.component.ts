38e8be6251445c9e653a16f8459fab58d1d5e9dc
benny.meltzer <meltzerbenny@gmail.com>
2022-09-04 17:42:55 -0500

    more images

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import photos from '@assets/photogallery/photos.json'
+// const fs = require('fs')
+
+const dir = '../../../assets/photogallery/'
+// const files = fs.readdirSync(dir)
+
 
 @Component({
   selector: 'app-gallery',
@@ -13,7 +18,11 @@ export class GalleryComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // for (const file of files) {
+    //   console.log(file)
+    // }
     for(let key in photos){
+      console.log(key, photos[key]);
       this.images.push({
         path: this.gallery_dir + photos[key]
       });
